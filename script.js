7a1ab772a898e9160430ac6ba2a2b93efea9f020
bossbadi <bossbadi123@gmail.com>
2022-11-13 12:06:38 -0500

    Update answer preview and paywall msg removal

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 // unblur answers
-document.querySelectorAll('div').forEach((div) => {
-    if (div.className.match(/^_[0-9a-z]{8}$/)) {
-        div.removeAttribute("class");
-    }
+document.querySelectorAll("div").forEach((div) => {
+  if (div.className.match(/^_[0-9a-z]{8}$/)) {
+    div.removeAttribute("class");
+  }
 });
 
 // remove answer preview
-document.getElementsByClassName("c-answer__body")[0].getElementsByTagName("p")[0].remove();
+var paras = document.getElementById("enotes-paywall").parentElement.childNodes;
+for (var i = 0; i < paras.length; i++) {
+  if (paras[i].tagName == "P") {
+    paras[i].remove();
+  }
+}
 
 // remove paywall message
-document.getElementById("enotes-paywall").parentNode.remove();
+document.getElementById("enotes-paywall").remove();
